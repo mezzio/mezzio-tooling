f23e06e289e4c2283594f5c6bf8c5d00d4a0c25d
Matthew Setter <matthew@matthewsetter.com>
2022-07-13 18:21:37 +0200

    Add classes to sort a list of Routes by name and path
    These two classes sort a traversable list of Route objects by both name
    and path in ascending order.

---
+++ test/Routes/Sorter/RouteSorterByNameTest.php
@@ -0,0 +1,101 @@
+<?php
+declare(strict_types=1);
+
+namespace MezzioTest\Tooling\Routes\Sorter;
+
+use Mezzio\Router\Route;
+use Mezzio\Tooling\Routes\Sorter\RouteSorterByName;
+use MezzioTest\Tooling\Routes\Middleware\ExpressMiddleware;
+use MezzioTest\Tooling\Routes\Middleware\SimpleMiddleware;
+use PHPUnit\Framework\TestCase;
+
+class RouteSorterByNameTest extends TestCase
+{
+    /**
+     * @var Route[]
+     */
+    private array $routes;
+
+    public function setUp(): void
+    {
+        $this->routes = [
+            new Route(
+                "/user/profile",
+                new SimpleMiddleware(),
+                ['GET'],
+                'user.profile'
+            ),
+            new Route(
+                "/",
+                new ExpressMiddleware(),
+                ['GET'],
+                'home'
+            ),
+            new Route(
+                "/login",
+                new ExpressMiddleware(),
+                ['GET'],
+                'user.login'
+            ),
+            new Route(
+                "/logout",
+                new ExpressMiddleware(),
+                ['GET'],
+                'user.logout'
+            ),
+            new Route(
+                "/logout",
+                new ExpressMiddleware(),
+                ['GET', 'POST'],
+                'user.logout'
+            ),
+            new Route(
+                "/logout",
+                new ExpressMiddleware(),
+                Route::HTTP_METHOD_ANY,
+                'user.logout'
+            ),
+        ];
+    }
+
+    public function testCanSortRoutesByName()
+    {
+        $sorter = new RouteSorterByName();
+        usort($this->routes, $sorter);
+
+        $this->assertCount(6, $this->routes);
+    }
+
+    public function testCanSortRoutesByNameInAscendingOrderOfName()
+    {
+        $sorter = new RouteSorterByName();
+        usort($this->routes, $sorter);
+
+        $this->assertCount(6, $this->routes);
+
+        $sortedPaths = [
+            'home',
+            'user.login',
+            'user.logout',
+            'user.logout',
+            'user.logout',
+            'user.profile',
+        ];
+        for ($i = 0; $i < count($sortedPaths); $i++) {
+            $this->assertSame(
+                $sortedPaths[$i],
+                $this->routes[$i]->getName(),
+                sprintf("Names for element %d don't match", $i)
+            );
+        }
+    }
+
+    public function testWillReturnAnEmptyArrayIfNoRoutesAreProvided()
+    {
+        $routes = [];
+        $sorter = new RouteSorterByName();
+        usort($routes, $sorter);
+
+        $this->assertEmpty($routes);
+    }
+}
