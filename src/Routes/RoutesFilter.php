b176e4527d3d92ffb17e11afb265a8f73636d6db
Matthew Setter <matthew@matthewsetter.com>
2022-07-13 14:47:00 +0200

    Add a class that can filter a list of Route objects
    This change adds a class that extends the FilterIterator from PHP's SPL.
    The intention of the class is to be able to filter the routes in an
    application's routing table by one or more of their four core
    properties/attributes, those being name, path, method(s), and
    middleware.
    
    Signed-off-by: Matthew Setter <matthew@matthewsetter.com>

---
+++ src/Routes/RoutesFilter.php
@@ -0,0 +1,106 @@
+<?php
+declare(strict_types=1);
+
+namespace Mezzio\Tooling\Routes;
+
+use Mezzio\Router\Route;
+
+/**
+ * RoutesFilter filters a traversable list of Route objects based on any of the four Route criteria,
+ * those being the route's name, path, middleware, or supported method(s).
+ */
+class RoutesFilter extends \FilterIterator
+{
+    /**
+     * An array storing the list of route options to filter a route on along with their respective values.
+     *
+     * The four allowed route options are: name, path, method, and middleware.
+     * Name and path can be a fixed string, such as user.profile, or a regular expression, such as user.*.
+     * Middleware can only contain a class name. Method can be either a string which contains one of the
+     * allowed HTTP methods, or an array of HTTP methods.
+     *
+     * @var array
+     */
+    private array $options;
+
+    public function __construct(\ArrayIterator $routes, array $options = [])
+    {
+        parent::__construct($routes);
+
+        $this->options = $options;
+    }
+
+    public function accept(): bool
+    {
+        /** @var Route $route */
+        $route = $this->getInnerIterator()->current();
+
+        if (!empty($this->options['name'])) {
+            return $route->getName() === $this->options['name'] || $this->matchesByRegex($route, 'name');
+        }
+
+        if (!empty($this->options['path'])) {
+            return $route->getPath() === $this->options['path'] || $this->matchesByRegex($route, 'path');
+        }
+
+        if (
+            (!empty($this->options['method']) || $this->options['method'] === Route::HTTP_METHOD_ANY) &&
+            $this->matchByMethod($route)
+        ) {
+            return true;
+        }
+
+        if (!empty($this->options['middleware'])) {
+            return get_class($route->getMiddleware()) === (string)$this->options['middleware'];
+        }
+
+        return false;
+    }
+
+    /**
+     * Match the route against a regular expression based on the field in $matchType.
+     *
+     * $matchType can be either "path" or "name".
+     *
+     * @return false|int
+     */
+    public function matchesByRegex(Route $route, string $routeAttribute)
+    {
+        if ($routeAttribute === 'path') {
+            $path = (string)$this->options['path'];
+            return preg_match(
+                sprintf("/^%s/", str_replace('/', '\/', $path)),
+                $route->getPath()
+            );
+        }
+
+        if ($routeAttribute === 'name') {
+            return preg_match(sprintf("/%s/", (string)$this->options['name']), $route->getName());
+        }
+    }
+
+    /**
+     * Match if the current route supports the method(s) supplied.
+     */
+    public function matchByMethod(Route $route): bool
+    {
+        if ($route->allowsAnyMethod()) {
+            return true;
+        }
+
+        if (is_string($this->options['method'])) {
+            return in_array(strtoupper($this->options['method']), $route->getAllowedMethods());
+        }
+
+        if (is_array($this->options['method'])) {
+            array_walk($this->options['method'], fn(&$value) => $value = strtoupper($value));
+            return
+                !empty(array_intersect(
+                    $this->options['method'],
+                    $route->getAllowedMethods()
+                ));
+        }
+
+        return false;
+    }
+}
---
+++ test/Routes/RoutesFilterTest.php
@@ -0,0 +1,174 @@
+<?php
+declare(strict_types=1);
+
+namespace MezzioTest\Tooling\Routes;
+
+use Mezzio\Router\Route;
+use Mezzio\Tooling\Routes\RoutesFilter;
+use MezzioTest\Tooling\Routes\Middleware\ExpressMiddleware;
+use MezzioTest\Tooling\Routes\Middleware\SimpleMiddleware;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+class RoutesFilterTest extends TestCase
+{
+    use ProphecyTrait;
+
+    private array $routes;
+
+    public function setUp(): void
+    {
+        $this->routes = [
+            new Route(
+                "/user/profile",
+                new SimpleMiddleware(),
+                ['GET'],
+                'user.profile'
+            ),
+            new Route(
+                "/",
+                new ExpressMiddleware(),
+                ['GET'],
+                'home'
+            ),
+            new Route(
+                "/login",
+                new ExpressMiddleware(),
+                ['GET'],
+                'user.login'
+            ),
+            new Route(
+                "/logout",
+                new ExpressMiddleware(),
+                ['GET'],
+                'user.logout'
+            ),
+            new Route(
+                "/logout",
+                new ExpressMiddleware(),
+                ['GET', 'POST'],
+                'user.logout'
+            ),
+            new Route(
+                "/logout",
+                new ExpressMiddleware(),
+                Route::HTTP_METHOD_ANY,
+                'user.logout'
+            ),
+        ];
+    }
+
+    /**
+     * @dataProvider validFilterDataProvider
+     */
+    public function testCanFilterRoutesProperly(int $expectedNumberOfRoutes, array $filterOptions = []): void
+    {
+        $this->setUp();
+
+        $routeFilter = new RoutesFilter(new \ArrayIterator($this->routes), $filterOptions);
+
+        $this->assertCount($expectedNumberOfRoutes, $routeFilter);
+    }
+
+    public function validFilterDataProvider(): array
+    {
+        return [
+            [
+                1,
+                [
+                    'name' => 'home',
+                ],
+            ],
+            [
+                5,
+                [
+                    'name' => 'user.*',
+                ],
+            ],
+            [
+                1,
+                [
+                    'path' => '/user',
+                ],
+            ],
+            [
+                4,
+                [
+                    'path' => '/log.*',
+                ],
+            ],
+            [
+                6,
+                [
+                    'path' => '/',
+                ],
+            ],
+            [
+                6,
+                [
+                    'method' => 'GET',
+                ],
+            ],
+            [
+                1,
+                [
+                    'method' => Route::HTTP_METHOD_ANY,
+                ],
+            ],
+            [
+                6,
+                [
+                    'method' => 'get',
+                ],
+            ],
+            [
+                2,
+                [
+                    'method' => 'post',
+                ],
+            ],
+            [
+                6,
+                [
+                    'method' => ['POST', 'GET'],
+                ],
+            ],
+            [
+                2,
+                [
+                    'method' => ['POST'],
+                ],
+            ],
+            [
+                6,
+                [
+                    'method' => ['GET'],
+                ],
+            ],
+            [
+                1,
+                [
+                    'method' => ['PATCH'],
+                ],
+            ],
+            [
+                2,
+                [
+                    'method' => ['PATCH', 'POST'],
+                ],
+            ],
+            [
+                2,
+                [
+                    'method' => ['patch', 'post'],
+                ],
+            ],
+            [
+                1,
+                [
+                    'method' => ['patch'],
+                ],
+            ],
+        ];
+    }
+}
