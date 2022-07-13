14109411fc38be9f68bcf986d8fd4e8aed991c15
Matthew Setter <matthew@matthewsetter.com>
2022-07-13 18:21:37 +0200

    Add classes to sort a list of Routes by name and path
    These two classes sort a traversable list of Route objects by both name
    and path in ascending order.
    
    Signed-off-by: Matthew Setter <matthew@matthewsetter.com>

---
+++ src/Routes/Sorter/RouteSorterByName.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Mezzio\Tooling\Routes\Sorter;
+
+use Mezzio\Router\Route;
+
+class RouteSorterByName
+{
+    public function __invoke(Route $routeOne, Route $routeTwo): int
+    {
+        return ($routeOne->getName() === $routeTwo->getName()) ? 0
+            : ($routeOne->getName() < $routeTwo->getName() ? -1 : 1);
+    }
+}
---
+++ test/Routes/Sorter/RouteSorterByPathTest.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+
+namespace MezzioTest\Tooling\Routes\Sorter;
+
+use Mezzio\Router\Route;
+use Mezzio\Tooling\Routes\Sorter\RouteSorterByPath;
+use MezzioTest\Tooling\Routes\Middleware\ExpressMiddleware;
+use MezzioTest\Tooling\Routes\Middleware\SimpleMiddleware;
+use PHPUnit\Framework\TestCase;
+
+class RouteSorterByPathTest extends TestCase
+{
+    /**
+     * @var Route[]
+     */
+    private array $routes;
+
+    public function setUp(): void
+    {
+        $this->routes = [
+            new Route(
+                "/user/profile",
+                new SimpleMiddleware(),
+                ['GET'],
+                'user.profile'
+            ),
+            new Route(
+                "/",
+                new ExpressMiddleware(),
+                ['GET'],
+                'home'
+            ),
+            new Route(
+                "/login",
+                new ExpressMiddleware(),
+                ['GET'],
+                'user.login'
+            ),
+            new Route(
+                "/logout",
+                new ExpressMiddleware(),
+                ['GET'],
+                'user.logout'
+            ),
+            new Route(
+                "/logout",
+                new ExpressMiddleware(),
+                ['GET', 'POST'],
+                'user.logout'
+            ),
+            new Route(
+                "/logout",
+                new ExpressMiddleware(),
+                Route::HTTP_METHOD_ANY,
+                'user.logout'
+            ),
+        ];
+    }
+
+    public function testCanSortRoutesByNameInAscendingOrderOfName()
+    {
+        $sorter = new RouteSorterByPath();
+        usort($this->routes, $sorter);
+
+        $this->assertCount(6, $this->routes);
+
+        $sortedPaths = [
+            '/',
+            '/login',
+            '/logout',
+            '/logout',
+            '/logout',
+            '/user/profile',
+        ];
+        for ($i = 0; $i < count($sortedPaths); $i++) {
+            $this->assertSame($sortedPaths[$i], $this->routes[$i]->getPath(), sprintf("Paths for element %d don't match", $i));
+        }
+    }
+
+    public function testWillReturnAnEmptyArrayIfNoRoutesAreProvided()
+    {
+        $routes = [];
+        $sorter = new RouteSorterByPath();
+        usort($routes, $sorter);
+
+        $this->assertEmpty($routes);
+    }
+}
