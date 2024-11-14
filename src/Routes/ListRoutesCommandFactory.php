d9d312b8467c73345fe5802a5de21462a369833c
Matthew Setter <matthew@matthewsetter.com>
2024-11-14 21:45:41 +1000

    Refactor how ListRoutesCommandFactory retrieves a ConfigLoaderInterface
    This change refactors ListRoutesCommandFactory to retrieve a
    ConfigLoaderInterface object from the application's DiC instead of
    instantiating a RoutesFileConfigLoader object directly. The main reason
    for this is to provide flexibility to the user, should they have the
    need to retrieve routes in a different way than the default
    implementation, which searches in config/routes.php and in any routes
    registered through ConfigProvider classes.
    
    Signed-off-by: Matthew Setter <matthew@matthewsetter.com>

--- src/Routes/ListRoutesCommandFactory.php
+++ src/Routes/ListRoutesCommandFactory.php
@@ -4,32 +4,19 @@ declare(strict_types=1);
 
 namespace Mezzio\Tooling\Routes;
 
-use Mezzio\Application;
-use Mezzio\MiddlewareFactory;
 use Mezzio\Router\RouteCollector;
 use Mezzio\Tooling\Routes\Filter\EmptyRouteFilterOptions;
-use Mezzio\Tooling\Routes\RoutesFileConfigLoader;
 use Psr\Container\ContainerInterface;
 
 final class ListRoutesCommandFactory
 {
     public function __invoke(ContainerInterface $container): ListRoutesCommand
     {
-        /** @var Application $application */
-        $application = $container->get(Application::class);
-
-        /** @var MiddlewareFactory $factory */
-        $factory = $container->get(MiddlewareFactory::class);
-
-        $configLoader = new RoutesFileConfigLoader(
-            'config/routes.php',
-            $application,
-            $factory,
-            $container
-        );
-
         /** @var RouteCollector $routeCollector */
         $routeCollector = $container->get(RouteCollector::class);
+
+        /** @var ConfigLoaderInterface $configLoader */
+        $configLoader = $container->get(ConfigLoaderInterface::class);
 
         return new ListRoutesCommand($routeCollector, $configLoader, new EmptyRouteFilterOptions());
     }
